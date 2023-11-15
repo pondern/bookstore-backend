e8561c18ba2d77399518fea361188852479e09c3
pondern <nicolasponder@gmail.com>
2023-11-15 14:01:27 -0500

    removing commented out lines

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,14 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-import User from "../models/User.js"
+import User from "../models/User.js";
 
 let SALT_ROUNDS = 11;
 let TOKEN_KEY = "";
 
 if (process.env.NODE_ENV === "production") {
-    SALT_ROUNDS = Number(process.env.SALT_ROUNDS);
-    TOKEN_KEY = process.env.TOKEN_KEY;
+  SALT_ROUNDS = Number(process.env.SALT_ROUNDS);
+  TOKEN_KEY = process.env.TOKEN_KEY;
 }
 
 const today = new Date();
@@ -16,97 +16,66 @@ const exp = new Date(today);
 exp.setDate(today.getDate() + 30);
 
 export const signUp = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const password_digest = await bcrypt.hash(password, SALT_ROUNDS);
-        const user = new User({
-            username,
-            email,
-            password_digest,
-        });
+  try {
+    const { username, email, password } = req.body;
+    const password_digest = await bcrypt.hash(password, SALT_ROUNDS);
+    const user = new User({
+      username,
+      email,
+      password_digest,
+    });
 
-        await user.save();
+    await user.save();
 
     const payload = {
       id: user._id,
       username: user.username,
       email: user.email,
       exp: parseInt(exp.getTime() / 1000),
-     };
+    };
     const token = jwt.sign(payload, TOKEN_KEY);
     res.status(201).json({ token });
- }  catch (error) {
+  } catch (error) {
     console.log(error.message);
-    res.status(400).json({ error: error.message});
- }
+    res.status(400).json({ error: error.message });
+  }
 };
 
 export const signIn = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email: email }).select(
-            "username email password_digest"
-        );
-        if (await bcrypt.compare(password, user.password_digest)) {
-            const payload = {
-                id: user._id,
-                username: user.username,
-                email: user.email,
-                exp: parseInt(exp.getTime() / 1000),
-            };
-            const token = jwt.sign(payload, TOKEN_KEY);
-            res.status(201).json({ token });
-        } else {
-            res.status(401).send("Invalid Credentials");
-        }
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ error: error.message });
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email }).select(
+      "username email password_digest"
+    );
+    if (await bcrypt.compare(password, user.password_digest)) {
+      const payload = {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        exp: parseInt(exp.getTime() / 1000),
+      };
+      const token = jwt.sign(payload, TOKEN_KEY);
+      res.status(201).json({ token });
+    } else {
+      res.status(401).send("Invalid Credentials");
     }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const verify = async (req, res) => {
-    try {
-      const token = req.headers.authorization.split(" ")[1];
-      const payload = jwt.verify(token, TOKEN_KEY);
-      if (payload) {
-        res.json(payload);
-      }
-    } catch (error) {
-      console.log(error.message);
-      res.status(401).send("Not Authorized");
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const payload = jwt.verify(token, TOKEN_KEY);
+    if (payload) {
+      res.json(payload);
     }
-  };
-  
-  export const changePassword = async (req, res) => {};
-  
+  } catch (error) {
+    console.log(error.message);
+    res.status(401).send("Not Authorized");
+  }
+};
 
-
-// export const updateBook = async (req, res) => {
-//   try {
-//     const { title } = req.params;
-//     const book = await Book.findByTitleAndUpdate(title, req.body);
-
-//     res.status(201).json(book);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// export const deleteBook = async (req, res) => {
-//   try {
-//     const { title } = req.params;
-//     const deleted = await Book.findByTitleAndDelete(title);
-
-//     if (deleted) {
-//       return res.status(200).send("Book deleted!");
-//     }
-
-//     throw new Error("Book not found!");
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
+export const changePassword = async (req, res) => {};
