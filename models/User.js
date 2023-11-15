a466be753c5a185edaefa4d214ee0661cef095ac
Ryan Gelman <ryangelman@MacBook-Pro-de-Ryan.local>
2023-11-15 13:10:19 -0500

    created users controller and model plus updated the route

--- models/User.js
+++ models/User.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-    name: { type: String },
-    email: { type: String },
-    password: { type: String },
+    username: { type: String, required: true,},
+    email: { type: String, required: true, },
+    password_digest: { type: String, required: true, select: false },
     library: [{
         book: { type: Schema.Types.ObjectId, ref: 'Book' },
         stars: { type: Number, min: 1, max: 5 },
