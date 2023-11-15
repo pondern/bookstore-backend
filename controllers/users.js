64018401cd02cbde3ac227de4d790f5bed1eba99
pondern <nicolasponder@gmail.com>
2023-11-15 09:36:26 -0500

    testing controllers & add genre to model/Books.js

---
+++ controllers/users.js
@@ -0,0 +1,61 @@
+import Book from "../models/Book.js";
+
+export const getUser = async (req, res) => {
+  return res.send("hello");
+};
+//** */ Dummy functions below for testing, deletion upon final testing
+
+// export const getBook = async (req, res) => {
+//   try {
+//     const { title } = req.params;
+//     const book = await Book.findBookByTitle(title);
+
+//     if (book) {
+//       return res.json(book);
+//     }
+
+//     res.stauts(404).json({ message: "Book not found!" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
+
+// export const createBook = async (req, res) => {
+//   try {
+//     const book = new Book(req.body);
+//     await book.save();
+//     res.status(201).json(book);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
+
+// export const updateBook = async (req, res) => {
+//   try {
+//     const { title } = req.params;
+//     const book = await Book.findByTitleAndUpdate(title, req.body);
+
+//     res.status(201).json(book);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
+
+// export const deleteBook = async (req, res) => {
+//   try {
+//     const { title } = req.params;
+//     const deleted = await Book.findByTitleAndDelete(title);
+
+//     if (deleted) {
+//       return res.status(200).send("Book deleted!");
+//     }
+
+//     throw new Error("Book not found!");
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
