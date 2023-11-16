7802226a1d065a71cf3ea9122eed57bf2ea7bc1d
pondern <nicolasponder@gmail.com>
2023-11-16 12:42:24 -0500

    remove dupes and add display name full  string

--- seed/data.js
+++ seed/data.js
@@ -5,79 +5,85 @@ import Library from "../models/Library.js";
 import collections from "./books.json" assert { type: "json" };
 import chalk from "chalk";
 import bcrypt from "bcrypt";
-
+let scrubbedBooks = [];
 const insertData = async () => {
-  await db.dropDatabase();
+  try {
+    await db.dropDatabase();
 
-  let scrubbedBooks = [];
-
-  collections.forEach((collection) => {
-    let splitName = collection.display_name.split(" ");
-    let display_name = splitName[splitName.length - 1];
-
-    collection.books.forEach((book) => {
-      scrubbedBooks.push({
-        display_name,
-        title: book.title,
-        author: book.author,
-        book_image: book.book_image,
-        publisher: book.publisher,
-        description: book.description,
-        buy_links: book.buy_links,
-        rank: book.rank,
-        weeks_on_list: book.weeks_on_list,
+    collections.forEach((collection) => {
+      collection.books.forEach((book) => {
+        scrubbedBooks.push({
+          display_name: collection.display_name,
+          title: book.title,
+          author: book.author,
+          book_image: book.book_image,
+          publisher: book.publisher,
+          description: book.description,
+          buy_links: book.buy_links,
+          rank: book.rank,
+          weeks_on_list: book.weeks_on_list,
+        });
       });
     });
-  });
 
-  await Book.create(scrubbedBooks);
+    const titleSet = new Set();
+    scrubbedBooks = scrubbedBooks.filter((book) => {
+      const isDuplicate = titleSet.has(book.title);
+      titleSet.add(book.title);
+      return !isDuplicate;
+    });
+    await Book.create(scrubbedBooks);
 
-  const user1 = new User({
-    username: "mike",
-    email: "mike@super.gmail.com",
-    password_digest: await bcrypt.hash("xyz123", 11),
-  });
-
-  await user1.save();
-
-  const user1Library = new Library({
-    books: [],
-    userId: user1._id,
-  });
-
-  await user1Library.save();
-
-  const user2 = new User({
-    username: "raul",
-    email: "ra@super.gmail.com",
-    password_digest: await bcrypt.hash("xyz123", 11),
-  });
-  await user2.save();
-
-  const user2Library = new Library({
-    books: [],
-    userId: user2._id,
-  });
-
-  await user2Library.save();
-
-  const user3 = new User({
-    username: "enzo",
-    email: "enzo@super.gmail.com",
-    password_digest: await bcrypt.hash("xyz123", 11),
-  });
-  await user3.save();
-
-  const user3Library = new Library({
-    books: [],
-    userId: user3._id,
-  });
-
-  await user3Library.save();
-
-  console.log(chalk.magenta("Books and Users created!"));
-
-  await db.close();
+    console.log(chalk.green("Database seeded successfully"));
+  } catch (error) {
+    console.error(chalk.red(`Error seeding database: ${error.message}`));
+  } finally {
+    await db.close();
+  }
 };
 
+const user1 = new User({
+  username: "mike",
+  email: "mike@super.gmail.com",
+  password_digest: await bcrypt.hash("xyz123", 11),
+});
+
+await user1.save();
+
+const user1Library = new Library({
+  books: [],
+  userId: user1._id,
+});
+
+await user1Library.save();
+
+const user2 = new User({
+  username: "raul",
+  email: "ra@super.gmail.com",
+  password_digest: await bcrypt.hash("xyz123", 11),
+});
+await user2.save();
+
+const user2Library = new Library({
+  books: [],
+  userId: user2._id,
+});
+
+await user2Library.save();
+
+const user3 = new User({
+  username: "enzo",
+  email: "enzo@super.gmail.com",
+  password_digest: await bcrypt.hash("xyz123", 11),
+});
+await user3.save();
+
+const user3Library = new Library({
+  books: [],
+  userId: user3._id,
+});
+
+await user3Library.save();
+
 insertData();
+console.log(chalk.magenta("Books and Users created!"));
